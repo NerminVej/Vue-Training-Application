2ed216750d72d890b02bbc99ba5273b620cca442
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-08 09:37:05 +0200

    installed tailwindcss

---
+++ nuxt_test/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+      <h1>Home Page</h1>
+      <nuxt-link to="/about">Go to About</nuxt-link>
+    </div>
+  </template>
+
+<script>
+
+</script>
--- vue_training/postcss.config.js
+++ vue_training/postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   },
-}
+})
