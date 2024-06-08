0177666ece7eacffac4da7c962b41bb768d8c77f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-06-08 09:51:48 +0200

    added some routes

--- nuxt_test/app.vue
+++ nuxt_test/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold">hi</h1>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ nuxt_test/pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h1>About Page</h1>
+      <nuxt-link to="/about/me">Go to About Me</nuxt-link>
+
+    </div>
+</template>
+
+
+<script>
+</script>
